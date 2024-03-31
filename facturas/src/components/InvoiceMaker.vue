0965bcf6da5d5e6ba85a89fec757c6f7e7bffb0d
g4nd4lf <rafaelromerovicente@gmail.com>
2024-04-01 01:51:53 +0200

    styling table and change words to english

--- facturas/src/components/InvoiceMaker.vue
+++ facturas/src/components/InvoiceMaker.vue
@@ -1,11 +1,16 @@
 <template>
-    <h1>Facturas</h1>
-    <input type="text" v-model="article.item"/>
-    <input type="text" v-model="article.quantity"/>
-    <input type="text" v-model="article.price"/>
-    <input type="text" v-model="article.totalPrice"/>
-    <button @click="addItem()">  Agregar   </button>
-    <table>
+    <h1>Invoice</h1>
+    <div class="form">
+        <label>Item</label>
+        <label>Quantity</label>
+        <label>Price</label>
+        <div></div>
+        <input ref="itemInput" type="text" v-model="article.item" @keydown.enter="addItem"/>
+        <input type="text" v-model="article.quantity" @keydown.enter="addItem"/>
+        <input type="text" v-model="article.price" @keydown.enter="addItem"/>
+        <button @click="addItem()">  Add   </button>
+    </div>
+    <table v-show="notEmpty">
         <tr>
             <th>Item</th>
             <th>Quantity</th>
@@ -13,17 +18,18 @@
             <th>Total Price</th>
         </tr>
         <tr v-for="(elemArticle,id) in articles" :key="id">
-            <th>{{elemArticle.item}}</th>
-            <th>{{elemArticle.quantity}}</th>
-            <th>{{elemArticle.price}}</th>
-            <th>{{elemArticle.totalPrice}}</th>
+            <td>{{elemArticle.item}}</td>
+            <td>{{elemArticle.quantity}}</td>
+            <td>{{elemArticle.price}}</td>
+            <td>{{elemArticle.totalPrice}}</td>
         </tr>
     </table>
-    <div>{{ article.item }}</div> 
-    <div>{{ article.price }}</div>
+
 </template>
 <script lang="ts" setup>
     import {ref, type Ref, type ComputedRef, computed} from 'vue'
+    const itemInput = ref<HTMLInputElement | null>(null); // Ref for the item input element
+
     interface IArticle {
         item: string,
         quantity: string,
@@ -38,6 +44,9 @@
         }
         )
     let articles:Ref<Array<IArticle>> =ref([])
+    
+    const notEmpty = computed(() => articles.value.length)
+
     const addItem=()=>{
         articles.value.push({
           item: article.value.item,
@@ -45,7 +54,28 @@
           price: article.value.price,
           totalPrice: (parseFloat(article.value.quantity)*parseFloat(article.value.price)).toString()
         })
+        article.value={item:"",quantity:"",price:"",totalPrice:""}
+        if (itemInput.value){
+            itemInput.value.focus();
+        }
     }
 </script>
-<style>
+<style scoped>
+.form{
+    color: rgb(28, 15, 50);
+    display: grid;
+    grid-template-columns: repeat(4,100px);
+}
+table {
+  border-collapse: collapse;
+  width: 100%;
+}
+
+th, td {
+  padding: 8px;
+  text-align: left;
+  border-bottom: 1px solid #DDD;
+}
+
+tr:hover {background-color: #D6EEEE;}
 </style>
